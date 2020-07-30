7280fa915168652f2b622ad068ab22f11de71f5e
neimengtian <674682228@qq.com>
2020-07-30 16:34:09 +0800

    first_code init

--- first_blood/src/www/study/com/base/Base01.java
+++ first_blood/src/www/study/com/base/Base01.java
@@ -1,7 +1,13 @@
 package www.study.com.base;
-
+import java.util.*;
 public class Base01 {
     public static void main(String[] args) {
-
+        System.out.println("today is:");
+        System.out.println(new Date());
+        int x = 1, y = 2, z = 3;
+        int a = x + y - 2/2 + z; // [1]
+        int b = x + (y - 2)/(2 + z); // [2]
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
