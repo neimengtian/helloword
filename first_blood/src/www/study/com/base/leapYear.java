21b6050066a98b8c3a8a010f14dccb6f1fa9aea7
neimengtian <674682228@qq.com>
2020-08-13 14:37:08 +0800

    leapYear finish

--- first_blood/src/www/study/com/base/leapYear.java
+++ first_blood/src/www/study/com/base/leapYear.java
@@ -5,10 +5,19 @@ import java.util.Scanner;
 public class leapYear {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
+        //String name = sc.nextLine();
+        //System.out.println(name);
         int year = sc.nextInt();
-        System.out.println(name);
         System.out.println(year);
+        if (year<=0){
+            System.out.println("game over");
+        }
+        if (year%4==0&&year%100!=0||year%400==0){
+            System.out.println("is leap year");
+        }else{
+            System.out.println("not leap year");
+        }
+
 
     }
 }
