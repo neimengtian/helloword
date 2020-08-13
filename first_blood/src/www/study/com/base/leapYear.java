418709e94c615beb45f06746d90a3f714aa47d48
neimengtian <674682228@qq.com>
2020-08-13 14:29:18 +0800

    leapYear

---
+++ first_blood/src/www/study/com/base/leapYear.java
@@ -0,0 +1,14 @@
+package www.study.com.base;
+
+import java.util.Scanner;
+
+public class leapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int year = sc.nextInt();
+        System.out.println(name);
+        System.out.println(year);
+
+    }
+}
